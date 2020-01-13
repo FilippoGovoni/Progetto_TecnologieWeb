9176f7ee0a7acb93c9744a0f0e9390f4b3ae376a
Filippo <andreagovoni@MacBook-Pro-di-Filippo.local>
2020-01-13 10:33:46 +0100

    modifica ricerca con date di defaulte modifiche grafiche

--- Progetto/resources/views/project/ore_progetto.blade.php
+++ Progetto/resources/views/project/ore_progetto.blade.php
@@ -1,32 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    #card {
+        float: left;
+    }
+</style>
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-6">
         <p>
-            <h1>Periodo selezionato</h1>
+            <h1>Periodo selezionato: <h3>{{ date('d/m/yy', strtotime($data_inizio)) }} - {{ date('d/m/yy', strtotime($data_fine)) }}</h3>
+            </h1>
         </p>
-        <p><h2><b>{{ $data_inizio }} - {{ $data_fine }}</b></h2></p>
-        <?php $totale=0;?>
-        @if(count($schede_ore)>0)
-            @foreach($progetti as $p)
-                @foreach($schede_ore as $s)
-                    @if($s->project_name == $p->name)
-                    <?php $totale = $totale + $s->hours_work; ?>
-                    @endif
-                @endforeach
-                <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{$p->name}}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{$p->description}}</h6>
-                    <p class="card-text">Totale ore spese sul progetto {{$totale}}</p>
-                </div>
-                </div>
-                <?php $totale=0;?>
-            @endforeach
-        @else
-            Non sono state inserite schede_ore nel periodo selezionato
-        @endif
     </div>
+</div>
+<div class="row">
+        <?php $totale = 0; ?>
+        @if(count($schede_ore)>0)
+        @foreach($progetti as $p)
+        @foreach($schede_ore as $s)
+        @if($s->project_name == $p->name)
+        <?php $totale = $totale + $s->hours_work; ?>
+        @endif
+        @endforeach
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">{{$p->name}}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{$p->description}}</h6>
+                <p class="card-text">Totale ore spese sul progetto {{$totale}}</p>
+            </div>
+        </div>
+        <?php $totale = 0; ?>
+        @endforeach
+        @else
+        Non sono state inserite schede_ore nel periodo selezionato
+        @endif
 </div>
 @endsection
--- Progetto/resources/views/project/research.blade.php
+++ Progetto/resources/views/project/research.blade.php
@@ -16,16 +16,16 @@
                     </ul>
                 </div>
             @endif
-
+            <?php $data = date('Y-m-d');?>
             <form action="{{ URL::action('ProjectController@ore_progetto') }}" method="GET">
                 
                 <div class="form-group">
                     <label for="data_inizio">Data di inizio</label>
-                    <input class="form-control" type="date" name="data_inizio" />
+                    <input class="form-control" type="date" name="data_inizio" value=<?php echo date('Y-m-01')?> />
                 </div>
                 <div class="form-group">
                     <label for="data_fine">Data di fine</label>
-                    <input class="form-control" type="date" name="data_fine" />
+                    <input class="form-control" type="date" name="data_fine" value=<?php echo date("Y-m-t", strtotime($data))?> />
                 </div>
                 <div>
                     <input class="btn btn-primary" type="submit" value="Cerca">
